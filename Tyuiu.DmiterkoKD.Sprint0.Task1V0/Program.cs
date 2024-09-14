d9610df0e214802a59143dd58a72bb53813a01c7
DmiterkoKD <kiradmiterko@gmail.com>
2024-09-14 16:00:44 +0500

    Task1

---
+++ Tyuiu.DmiterkoKD.Sprint0.Task1V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.DmiterkoKD.Sprint0.Task1V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Kira";
+            int age = 18;
+            Console.WriteLine("Hello, my name is " + name + ". I`m " + age + " years ago");
+            Console.ReadKey();
+        }
+    }
+}
