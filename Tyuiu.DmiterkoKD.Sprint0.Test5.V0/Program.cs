4606b07f0fbba62193498c4547fbd2602f05b803
DmiterkoKD <kiradmiterko@gmail.com>
2024-09-16 22:04:38 +0500

    Task5

--- Tyuiu.DmiterkoKD.Sprint0.Test5.V0/Program.cs
+++ Tyuiu.DmiterkoKD.Sprint0.Test5.V0/Program.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.DmiterkoKD.Sprint0.Test5.V0
             Console.WriteLine("A+B =" + DataService.Addition(1, 5));
             Console.WriteLine("A-B = " + DataService.Subtration(15, 5));
             Console.WriteLine("A*B = " + DataService.Multiplicion(10, 10));
-            Console.WriteLine("A/B = " + DataService.Division(9, 3));
+            Console.WriteLine("A/B = " + DataService.Division(15, 3));
         }
     }
 }
