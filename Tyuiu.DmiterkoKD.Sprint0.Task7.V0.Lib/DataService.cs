5ea0be38ad186b11e40ca73063ecb780aaf6d151
DmiterkoKD <kiradmiterko@gmail.com>
2024-09-19 20:34:36 +0500

    Task7

---
+++ Tyuiu.DmiterkoKD.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.DmiterkoKD.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] num0n, int[] numTwo)
+        {
+            int[] resultArray = new int[5];
+            for (var i = 0; i < resultArray.Length; i++)
+            {
+                resultArray[i] = num0n[i] + numTwo[i];
+            }
+            return resultArray;
+        }
+    }
+}
