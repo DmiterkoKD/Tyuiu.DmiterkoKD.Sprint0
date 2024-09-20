cea6b4cda2727ca05289b229761ec288a1766ea5
DmiterkoKD <kiradmiterko@gmail.com>
2024-09-20 11:51:55 +0500

    Task5

--- Tyuiu.DmiterkoKD.Sprint0.Test5.V0/Program.cs
+++ Tyuiu.DmiterkoKD.Sprint0.Test5.V0/Program.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.DmiterkoKD.Sprint0.Test5.V0
         static void Main(string[] args)
         {
 
-            Console.WriteLine("A+B =" + DataService.Addition(1, 5));
+            Console.WriteLine("A+B =" + DataService.Addition(3, 5));
             Console.WriteLine("A-B = " + DataService.Subtration(15, 5));
             Console.WriteLine("A*B = " + DataService.Multiplicion(10, 10));
             Console.WriteLine("A/B = " + DataService.Division(15, 3));
